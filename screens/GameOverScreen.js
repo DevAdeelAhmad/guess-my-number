48fba5312d2fd5c832c81c51acc978eb42be6578
Adeel Ahmad <devadeelahmad@gmail.com>
2023-09-18 18:42:59 +0500

    Working on Game Over Screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,24 +1,63 @@
-import {Text, View, StyleSheet} from 'react-native'
+import { View, StyleSheet, Image, Text } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../utils/colors";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-function GameOverScreen(){
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>The Game is Over!</Text>
-        </View>
-    )
+function GameOverScreen() {
+  return (
+    <View style={styles.container}>
+      <Title>The Game is Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text style={styles.resultText}>
+        Your device needed <Text style={styles.resultHighlightText}>X</Text>{" "}
+        rounds to guess the number{" "}
+        <Text style={styles.resultHighlightText}>Y</Text>
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
+    </View>
+  );
 }
 
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text:{
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: 'white',
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding:20,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  resultText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  resultHighlightText: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
